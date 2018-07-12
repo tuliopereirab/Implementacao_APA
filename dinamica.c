30386adca48a271962a7610c7261f295d9755bf4
Tulio <tulio.pereirab@gmail.com>
2018-07-12 16:55:37 -0300

    Programação dinâmica funcional 	- Implementado também escolha se deseja imprimir matrizes maiores que [20][20] 	- Outras matrizes (custoBeneficio e Status) comentadas para não serem impressas 	- Só está imprimindo matriz de custos básica

--- dinamica.c
+++ dinamica.c
@@ -21,6 +21,7 @@ int **matriz;
 int i, j;
 int verificarVerticesDin(char passouPor[], int index, int numVertices);
 int contarVerticesDin(char passouPor[]);
+int somatorioDistancias=0;
 
 struct _matrizCustos **matrizCustos;
 
@@ -29,7 +30,8 @@ int *vetPassados;
 
 
 //int** matrizCustos;
-void melhorCaminho();
+int confirmacaoImpressao();
+int melhorCaminho();
 void termina();
 int verifFinal();
 void gerarCustos(int ii, int k);
@@ -40,6 +42,7 @@ void contadorVertices();
 
 void dinamica(char arquivo[]){
     int x, y;
+    int retorno;
     matriz = lerArquivo(arquivo);
     i = retI();
     j = retJ();
@@ -53,11 +56,16 @@ void dinamica(char arquivo[]){
     }
 
     contadorVertices();
-    imprimeMatriz();
     vetPassados = calloc(1, sizeof(int)*i);
 
     printf("Encontrando melhor caminho...\n");
-    melhorCaminho(1);
+    retorno = melhorCaminho(1);
+    for(x=0;x<i;x++) printf("%i ", vetPassados[x]);
+    while((verifFinal() != 0) && (retorno != 0))  // verifFinal: 0-> acabou!; 1-> segue            retorno: 0-> acabou, 1-> segue
+        retorno = melhorCaminho(0);
+
+    imprimeMatriz();
+    termina();
     system(EXIT_SUCCESS);
 }
 
@@ -105,88 +113,62 @@ void criaMatrizCustos(){
     printf("Matriz de custos criada...\n");
 }
 
-void imprimeMatriz(){
-    int x, y;
-    for(x=0;x<i;x++){
-        for(y=0;y<i;y++)
-            printf("%i\t", matrizCustos[x][y].custo);
-            //printf("%s ||", matrizCustos[x][y].passouPor);
-        printf("\n");
+int confirmacaoImpressao(){
+    char op;
+    printf("Matriz muito grande: [%i][%i]\nDeseja imprimir? [y/n] -> ", i, j);
+    scanf("%c", &op);
+    while(op != 'y' && op != 'n'){
+        printf("\nOpção digitada inválida!\nTente novamente.");
+        printf("\nDeseja imprimir matriz [%i][%i]? [y/n] -> ", i, j);
+        scanf("%c", &op);
     }
-    printf("\n--------------------------- VERTICES PASSADOS ---------------------------\n");
-    for(x=0;x<i;x++){
-        for(y=0;y<i;y++)
-            printf("%s\t", matrizCustos[x][y].passouPor);
-            //printf("%s ||", matrizCustos[x][y].passouPor);
-        printf("\n");
-    }
-    printf("\n--------------------------- CUSTO custoBeneficio ---------------------------\n");
-    for(x=0;x<i;x++){
-        for(y=0;y<i;y++)
-            printf("%.2f\t", matrizCustos[x][y].custoBeneficio);
-            //printf("%s ||", matrizCustos[x][y].passouPor);
-        printf("\n");
-    }
-    printf("\n--------------------------- NUM VERTICES ---------------------------\n");
-    for(x=0;x<i;x++){
-        for(y=0;y<i;y++)
-            printf("%i\t", matrizCustos[x][y].numVertices);
-            //printf("%s ||", matrizCustos[x][y].passouPor);
-        printf("\n");
-    }
-    printf("\n--------------------------- STATUS ---------------------------\n");
-    for(x=0;x<i;x++){
-        for(y=0;y<i;y++)
-            printf("%i\t", matrizCustos[x][y].status);
-            //printf("%s ||", matrizCustos[x][y].passouPor);
-        printf("\n");
-    }
+    if(op == 'y') return 1;
+    if(op == 'n') return 0;
+    return -1;
 }
 
-
-void imprimirMelhor(int final){
+void imprimeMatriz(){
     int x, y;
-    int statusFinal = 1;
-    int menorVal=-1;
-    int menorI=-1;
-    int menorJ=-1;
-    if(final == 0)
+    int verifImp;
+    if(i > 20) verifImp = confirmacaoImpressao();
+    else verifImp = 1;     // 1-> indica que é para imprimir, 0-> indica que não é pra imprimir, -1-> erro
+    if(verifImp == 1){
+        printf("\n--------------------------- CUSTO ---------------------------\n");
         for(x=0;x<i;x++){
-            printf("X= %i\n", x);
-            if(x+1<i){
-                for(y=x+1; y<i; y++){
-                    printf("y = %i\n", y);
-                    if(menorVal == -1){
-                        if(matrizCustos[x][y].usado == 0){
-                            printf("Menor val!\n");
-                            menorVal = matrizCustos[x][y].custo;
-                            menorI = x;
-                            menorJ = y;
-                            statusFinal = 0;
-                        }
-                    }else{
-                        if(matrizCustos[x][y].custo < menorVal){
-                            printf("Achou menor!\n");
-                            if(matrizCustos[x][y].usado == 0){
-                                printf("Usou!\n");
-                                menorVal = matrizCustos[x][y].custo;
-                                menorI = x;
-                                menorJ = y;
-                                statusFinal = 0;
-                            }
-                        }
-                    }
-                }
-            }
+            for(y=0;y<i;y++)
+                printf("%i\t", matrizCustos[x][y].custo);
+                //printf("%s ||", matrizCustos[x][y].passouPor);
+            printf("\n");
         }
-        strcat(caminhoFinal, matrizCustos[menorI][menorJ].passouPor);
-        strcat(caminhoFinal, " ");
-        matrizCustos[menorI][menorJ].usado = 1;
-        if(final == 0){
-            printf("Novamente!: %i\n", statusFinal);
-            imprimirMelhor(statusFinal);
-        }
-
+        // printf("\n--------------------------- VERTICES PASSADOS ---------------------------\n");
+        // for(x=0;x<i;x++){
+        //     for(y=0;y<i;y++)
+        //         printf("%s\t", matrizCustos[x][y].passouPor);
+        //         //printf("%s ||", matrizCustos[x][y].passouPor);
+        //     printf("\n");
+        // }
+        // printf("\n--------------------------- CUSTO custoBeneficio ---------------------------\n");
+        // for(x=0;x<i;x++){
+        //     for(y=0;y<i;y++)
+        //         printf("%.2f\t", matrizCustos[x][y].custoBeneficio);
+        //         //printf("%s ||", matrizCustos[x][y].passouPor);
+        //     printf("\n");
+        // }
+        // printf("\n--------------------------- NUM VERTICES ---------------------------\n");
+        // for(x=0;x<i;x++){
+        //     for(y=0;y<i;y++)
+        //         printf("%i\t", matrizCustos[x][y].numVertices);
+        //         //printf("%s ||", matrizCustos[x][y].passouPor);
+        //     printf("\n");
+        // }
+        // printf("\n--------------------------- STATUS ---------------------------\n");
+        // for(x=0;x<i;x++){
+        //     for(y=0;y<i;y++)
+        //         printf("%i\t", matrizCustos[x][y].status);
+        //         //printf("%s ||", matrizCustos[x][y].passouPor);
+        //     printf("\n");
+        // }
+    }
 }
 
 void contadorVertices(){
@@ -212,15 +194,12 @@ void contadorVertices(){
     }
 }
 
-void melhorCaminho(int inicio){
+int melhorCaminho(int inicio){
     int menorX=-1, menorY, y, x, statLeituraMatriz=0;   //0 nenhuma posição da matriz disponivel; 1 existe posição disponivel
     int numVertices, verticeUsado;
     int vertice;
     char auxVertice[15];
-    int status = verifFinal();      // 0-> acabou!; 1-> segue
     int menorX1=-1, menorX2=-1, menorY1=-1,menorY2=-1;
-    printf("Status: %i\n", status);
-    if(status == 0) termina();
     char *aux = malloc(sizeof(char)*i*4);
     if(inicio == 1){
         for(x=0;x<i;x++){
@@ -237,7 +216,6 @@ void melhorCaminho(int inicio){
                         if(matrizCustos[x][y].custoBeneficio < matrizCustos[menorX][menorY].custoBeneficio){
                             menorX = x;
                             menorY = y;
-                            printf("Vertice Custos: %i %i\n", x, y);
                         }
                     }
                 }
@@ -293,9 +271,8 @@ void melhorCaminho(int inicio){
             menorX = menorX1;
         }else{
             printf("Nao achou caminho!\n");
-            system(EXIT_SUCCESS);
+            return 0;
         }
-        printf("Menor X: %i\nMenor Y: %i\nMelhor Custo: %.2f\n", menorX, menorY, matrizCustos[menorX][menorY].custoBeneficio);
     }
     //if(statLeituraMatriz == 0) system(EXIT_SUCCESS);
     strcpy(aux, caminhoFinal);
@@ -310,13 +287,12 @@ void melhorCaminho(int inicio){
             }else{
                 printf("Não pode usar %i %i\n", menorX, menorY);
                 free(aux);
-                melhorCaminho(0);
+                return 1;
             }
         }
     }else{
         for(x=0;x<numVertices;x++){
             vertice = verificarVerticesDin(matrizCustos[menorX][menorY].passouPor, x, numVertices);
-            printf("Status Vertice: %i\nvertice: %i\n", vetPassados[vertice], vertice);
             if((vetPassados[vertice] == 0) || (vertice == verticeUsado)){
                 if(vertice != verticeUsado){
                     sprintf(auxVertice, " %i", vertice);
@@ -324,26 +300,26 @@ void melhorCaminho(int inicio){
                 }
             }else{
                 free(aux);
-                melhorCaminho(0);
+                return 1;
             }
         }
     }
-    printf("aux: %s\n", aux);
     strcpy(caminhoFinal, aux);
     printf("Caminho: %s\n", caminhoFinal);
     numVertices = contarVerticesDin(caminhoFinal);
-    for(x=0;x<numVertices;x++){
+    for(x=1;x<numVertices;x++){
         vertice = verificarVerticesDin(caminhoFinal, x, numVertices);
         vetPassados[vertice] = 1;
     }
     free(aux);
-    // for(x=0;x<i;x++){
-    //     for(y=0;y<i;y++)
-    //         printf("%i\t", matrizCustos[x][y].usado);
-    //         //printf("%s ||", matrizCustos[x][y].passouPor);
-    //     printf("\n");
-    // }
-    melhorCaminho(0);
+     // for(x=0;x<i;x++){
+     //     for(y=0;y<i;y++)
+     //         printf("%i\t", matrizCustos[x][y].usado);
+     //         //printf("%s ||", matrizCustos[x][y].passouPor);
+     //     printf("\n");
+     // }
+    somatorioDistancias = somatorioDistancias + matrizCustos[menorX][menorY].custo;
+    return 1;
 }
 
 int verifFinal(){
@@ -355,7 +331,9 @@ int verifFinal(){
 }
 
 void termina(){
-    printf("Melhor caminho: %s\n", caminhoFinal);
+    printf("------------------------------------------\n");
+    printf("Melhor caminho:\n%s\nSOMA: %i\n", caminhoFinal, somatorioDistancias);
+    printf("------------------------------------------\n");
     system(EXIT_SUCCESS);
 }
 
